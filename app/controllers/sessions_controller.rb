98d2aa39390ec0557c552b926903419b06597f90
deepali14111141 <Deepali.Worlikar@kellogg.northwestern.edu>
2022-03-12 05:02:53 +0000

    basic authentication done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.where({ email: params["email"]})[0]
+    if @user
+      if @user.password == params["password"]
+        flash[:notice] = "You logged in!"
+      redirect_to "/places"
+      else
+        flash[:notice] = "Wrong email id or password"
+        redirect_to "/sessions/new"
+      end
+    else
+      redirect_to "/sessions/new"
+    end
   end
+
 
   def destroy
   end
